2618e564e28b62fb8e0afe1956946a53bf65c176
Jacob Wolniewicz <jacob@wolniewicz.com>
2014-05-06 14:30:27 -0600

    making personal page be the one up here

---
+++ js/javascript.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+  $( "#dialog" ).dialog({
+    autoOpen: false,
+    show: {
+      effect: "blind",
+      duration: 1000
+    },
+    hide: {
+      effect: "explode",
+      duration: 1000
+    }
+  });
+
+  $( "#opener" ).click(function() {
+    $( "#dialog" ).dialog( "open" );
+  });
+
+  $('[rel=tooltip]').tooltip();});
